c5fb7c02296dc4d31d3ed566dd08a537d7b520ff
Nicolas Mateo Uroz <nicolasmateo1995@gmail.com>
2023-06-09 12:59:21 +0200

    ex 3-2

--- 03 - array/3-2.js
+++ 03 - array/3-2.js
@@ -1,5 +1,19 @@
-let arr = ["Apple", "Pear", "Banana"];
+let array1 = [1, 2, 3, 4, 5];
+let array2 = [100, 101, 102];
 
-for (let elem of arr) {
-  console.log("Do you want to eat a " + elem + "?");
+let sum = 0;
+let sum1 = 0;
+
+for(let elem of array1){
+    sum += elem;
 }
+
+for(let elem of array2){
+    sum1 += elem;
+}
+
+let average = sum / array1.length;
+let average1 = sum1 / array2.length;
+
+console.log("Average of array1: " + average);
+console.log("Average of array2: " + average1);
